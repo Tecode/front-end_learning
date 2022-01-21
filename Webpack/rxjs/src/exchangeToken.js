eaf85e9f0d2e28c2522a89ff0238b22f3f760b86
Tecode <283731869@qq.com>
2022-01-21 10:09:10 +0800

    axios请求

--- Webpack/rxjs/src/exchangeToken.js
+++ Webpack/rxjs/src/exchangeToken.js
@@ -1,8 +1,28 @@
 // import { userApi, productListApi, recommendedListApi } from "./mergeRequest";
+import { Observable } from "rxjs";
+import axios from "axios";
 
 const exchangeTokenApi = () =>
   new Promise((resolve, reject) => {
     resolve({ data: { token: "7EH9E6EJ6LL-@QW" } });
   });
-console.log("Api");
-exchangeTokenApi();
+// exchangeTokenApi();
+
+const observable$ = new Observable((subscriber) => {});
+
+axios.interceptors.request.use(
+  function (response) {
+    // Do something before request is sent
+    return response;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+axios.get("/api/refreshToken").then((data) => {
+  console.log(data);
+});
+
+console.log("Api2w");

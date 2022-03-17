265ef1675e53ec1343b9ec9d38e1069e23387601
Tecode <283731869@qq.com>
2022-03-17 22:55:03 +0800

    插件，自动构建

--- automate-webpack/haoxuan.config.js
+++ automate-webpack/haoxuan.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: "./src/index.js",
@@ -59,4 +62,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./index.html" })
+  ]
 };

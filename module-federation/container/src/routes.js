181b885b73bd2932968cbf70b7f36293704250a0
Tecode <283731869@qq.com>
2022-03-20 23:01:28 +0800

    解决路由问题

--- module-federation/container/src/routes.js
+++ module-federation/container/src/routes.js
@@ -4,9 +4,20 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/product/:name", name: "authChild", component: () => import(/* webpackChunkName: "group-authChild" */"./components/Product.vue") },
+  // { path: "/product/:name", name: "authChild", component: () => import(/* webpackChunkName: "group-authChild" */"./components/Product.vue") },
   { path: "/", name: "foo", component: () => import(/* webpackChunkName: "group-foo" */"auth/MainComponent") },
   { path: "/bar", name: "bar", component: () => import(/* webpackChunkName: "group-bar" */"product/HelloWorld") },
+  {
+    path: '/product',
+    name: 'product',
+    component:() => import("./components/HelloWorld.vue"),
+    children: [
+      {
+        path: ':pathMatch(.*)*',
+        component: () => import("./components/Product.vue")
+      }
+    ]
+  }
 ];
 
 const router = new VueRouter({ routes, mode: "history" });

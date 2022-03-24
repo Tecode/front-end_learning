37501e1d239807e576f9b5aaab47414a066ae8f2
Tecode <283731869@qq.com>
2022-03-24 19:44:36 +0800

    webpack模块热更新

--- front_end_advanced/009.webpack5/29-webpack-proxy/src/App.jsx
+++ front_end_advanced/009.webpack5/29-webpack-proxy/src/App.jsx
@@ -1,16 +1,16 @@
-import React, {Component} from 'react'
-import {BrowserRouter, Link, Route} from 'react-router-dom'
+import React, { Component } from 'react'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props)
     this.state = {
       title: '前端'
     }
   }
-  render(){
+  render() {
     return (
       <div>
         <h2>{this.state.title}</h2>

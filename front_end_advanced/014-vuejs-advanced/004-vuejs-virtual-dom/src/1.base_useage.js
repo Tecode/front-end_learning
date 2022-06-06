981444da65b8300e77b05605cb309a9d6b7dbbcc
Tecode <283731869@qq.com>
2022-06-07 00:03:37 +0800

    生命周期函数

--- front_end_advanced/014-vuejs-advanced/004-vuejs-virtual-dom/src/1.base_useage.js
+++ front_end_advanced/014-vuejs-advanced/004-vuejs-virtual-dom/src/1.base_useage.js
@@ -9,20 +9,30 @@ const app = document.querySelector('#app')
 // 第一个参数：标签+选择器
 // 第二个参数：如果是字符串就是标签中的文本内容
 const v_node001 = h('div#container.name', [
-    h('h1', 'Hello Snabbdom')
+    h('h1', {
+        hook: {
+            init(vnode) {
+                console.log(vnode.elm);
+            },
+            create(emptyNode, vnode) {
+                console.log(vnode.elm);
+            }
+        }
+    }, 'Hello Snabbdom')
 ])
 
 const patch001 = patch(app, v_node001)
 
-const v_node002 = h('h1#title.wrap_box', 'Edit Snabbdom')
+// const v_node002 = h('h1#title.wrap_box', 'Edit Snabbdom')
+// patch002 = patch(patch001, v_node002)
 
-let patch002 = null;
-setTimeout(() => {
-    patch002 = patch(patch001, v_node002)
-}, 2000)
+// let patch002 = null;
+// setTimeout(() => {
+//     patch002 = patch(patch001, v_node002)
+// }, 2000)
 
 // 清空div的内容
-const v_node003 = h('!')
-setTimeout(() => {
-    patch(patch002, v_node003)
-}, 4000)
+// const v_node003 = h('!')
+// setTimeout(() => {
+//     patch(patch002, v_node003)
+// }, 4000)

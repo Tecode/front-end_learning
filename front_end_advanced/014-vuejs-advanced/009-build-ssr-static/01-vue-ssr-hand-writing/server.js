3a18001b54f37b1f3de711ae023c097d11cdd0f6
Tecode <283731869@qq.com>
2022-07-04 21:53:07 +0800

    使用模版进行服务端渲染

--- front_end_advanced/014-vuejs-advanced/009-build-ssr-static/01-vue-ssr-hand-writing/server.js
+++ front_end_advanced/014-vuejs-advanced/009-build-ssr-static/01-vue-ssr-hand-writing/server.js
@@ -1,6 +1,9 @@
 const Vue = require("vue");
 const express = require("express");
-const renderer = require("vue-server-renderer").createRenderer();
+const fs = require("fs");
+const renderer = require("vue-server-renderer").createRenderer({
+  template: fs.readFileSync("./index.template.html", "utf-8"),
+});
 
 const server = express();
 
@@ -16,11 +19,21 @@ server.get("/", (req, res) => {
     },
   });
 
-  renderer.renderToString(app, (err, html) => {
-    console.log(html, "---");
-    res.setHeader()
-    res.send(html);
-  });
+  renderer.renderToString(
+    app,
+    {
+      title: "拉勾教育",
+      meta: `
+      <meta name="description" content="拉勾教育">
+    `,
+      url: req.url,
+    },
+    (err, html) => {
+      console.log(html, "---");
+      res.setHeader("Content-Type", "text/html; charset=utf8");
+      res.send(html);
+    }
+  );
 });
 
 server.listen(3000, () => console.log("Running...."));

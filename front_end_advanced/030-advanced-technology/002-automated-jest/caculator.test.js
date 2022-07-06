20e0ef1c09bb2a645d8ff40c4a6465015ef7ee0c
Tecode <283731869@qq.com>
2022-07-06 22:33:00 +0800

    jest配置

---
+++ front_end_advanced/030-advanced-technology/002-automated-jest/caculator.test.js
@@ -0,0 +1,14 @@
+const { sum, subtract } = require("./calculator")
+
+// 引入需要测试的函数
+// 给函数一个输出
+// 定义预期输出
+// 检查函数是否返回了预期的结果
+
+test('sum(1,2) 的结果应该是3', () => {
+    expect(sum(1, 2)).toBe(3)
+})
+
+test('subtract(1,2) 的结果应该是-1', () => {
+    expect(subtract(1, 2)).toBe(-1)
+})

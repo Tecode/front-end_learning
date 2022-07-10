3b2628401f9477395e74aab380db98fe54edc130
Tecode <283731869@qq.com>
2022-07-10 14:12:38 +0800

    vue and shnapshot test

--- front_end_advanced/030-advanced-technology/002-automated-jest/html.test.js
+++ front_end_advanced/030-advanced-technology/002-automated-jest/html.test.js
@@ -1,5 +1,6 @@
 // DOM测试
 // yarn jest --env=jsdom .\html.test.js
+import Vue from 'vue/dist/vue'
 
 function renderHtml() {
     const div = document.createElement('div')
@@ -11,4 +12,19 @@ test('DOM Test', () => {
     renderHtml()
     // console.log(document.body.innerHTML)
     expect(document.body.querySelector('h1').innerHTML).toBe('Hello World')
+ })
+
+// 测试Vue组件
+ function renderVueComponent(params) {
+    document.body.innerHTML = `<div id='app'></div>`
+    new Vue({
+        template: `<div>{{message}}</div>`,
+        data: {message: 'Hello World'}
+    }).$mount('#app')
+ }
+
+ test('Vue DOM Test', () => {
+    renderVueComponent()
+    // console.log(document.body.innerHTML)
+    expect(document.body.innerHTML).toMatch(/Hello World/)
  })
--- front_end_advanced/030-advanced-technology/002-automated-jest/ui.test.js
+++ front_end_advanced/030-advanced-technology/002-automated-jest/ui.test.js
@@ -1,3 +1,19 @@
-test('should first', () => { 
-    console.log('ui');
+import Vue from 'vue/dist/vue'
+
+function renderVueComponent(params) {
+    document.body.innerHTML = `<div id='app'></div>`
+    new Vue({
+        template: `<div id='#app1'><div>{{message}}</div></div>`,
+        data: {message: 'Hello World'}
+    }).$mount('#app')
+ }
+
+
+test('Snapshot Test', () => {
+    renderVueComponent()
+    // 第一次运行会生成快照文件字符串
+    // 下一次会对它和快照文件对比
+    expect(document.body.innerHTML).toMatchSnapshot()
  })
+
+//  yarn jest --env=jsdom --updateSnapshot 更新快照

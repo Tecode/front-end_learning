20b537f614d8f69eaee8837c3807ddc9164c66e8
Tecode <283731869@qq.com>
2022-11-23 21:58:12 +0800

    📃 docs:process

--- front_end_advanced/024-Node.js/01-Node.js-Advanced-programming/01-base-use/04-node-global.js
+++ front_end_advanced/024-Node.js/01-Node.js-Advanced-programming/01-base-use/04-node-global.js
@@ -1,20 +1,18 @@
-console.log(global)
+// console.log(global)
 
-/* console.log(__filename)
-console.log(__dirname)
+// console.log(__filename)
+// console.log(__dirname)
 
-console.log(this)
- */
+// console.log(this) // false
 // 默认情况 this 是空对象，和 global 并不是一样的
 
 // console.log(this == global)
+    (function () {
+        console.log(this == global) // true
+    })()
 
-//   (function () {
-//     console.log(this == global)
-//   })()
-
-/* require('module')
-__filename
-__dirname
-module 
-exports  */
+// require('module')
+// __filename
+// __dirname
+// module
+// exports  

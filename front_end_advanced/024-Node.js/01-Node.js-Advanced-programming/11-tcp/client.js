1f49dc92d0d4aa37d18a95fdb9fcef3069f9063d
Tecode <283731869@qq.com>
2023-01-31 20:47:57 +0800

    实现封包拆包

--- front_end_advanced/024-Node.js/01-Node.js-Advanced-programming/11-tcp/client.js
+++ front_end_advanced/024-Node.js/01-Node.js-Advanced-programming/11-tcp/client.js
@@ -15,6 +15,8 @@ client.write(ts.encode('拉勾教育3'))
 client.write(ts.encode('拉勾教育4'))
 client.write(ts.encode('拉勾教育5'))
 
+console.log(ts.encode('拉勾教育1'), '09090');
+// <Buffer 00 05 00 0d e6 8b 89 e5 8b be e6 95 99 e8 82 b2 31>
 client.on('data', (chunk) => {
   if (overageBuffer) {
     chunk = Buffer.concat([overageBuffer, chunk])
